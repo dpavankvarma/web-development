909767538dc0f804e5712cc7eef07e18806c02cc
Pavan Kumar Varma <dpavankvarma@gmail.com>
2019-10-05 15:00:36 +0530

    Refactored the code

--- 6_To-Do-List/list.js
+++ 6_To-Do-List/list.js
@@ -6,29 +6,41 @@ var input = prompt("What would like to do?");
 while (input !== "quit") {
     //handle input
     if (input === "list") {
-        console.log("***********");
-        todos.forEach(function(todo, i) {
-        console.log(i + " " + todo);
-       });
-       console.log("***********");
+        listTodos();    
     } 
     else if (input === "new") {
-        //ask for new todo
-        var newTodo = prompt("Enter new todo")
-        //add to todos array
-        todos.push(newTodo);
-        console.log("Added Todo");
+        addTodo();        
     }
     else if (input === "delete") {
-        //ask for index of todo to be deleted
-        var index = prompt("Enter index of todo to delete");
-        
-        //delete that todo
-        todos.splice(index,1);
-        console.log("Deleted Todo");
+        deleteTodo();
     }
     // ask again for new input
-    var input = prompt("What would like to do?");
+      input = prompt("What would like to do?");
 }
 
 console.log("OK, YOUR QUIT THE APP");
+
+function listTodos(){
+    console.log("***********");
+    todos.forEach(function(todo, i) {
+    console.log(i + " " + todo);
+   });
+   console.log("***********");    
+}
+
+function addTodo() {
+    //ask for new todo
+    var newTodo = prompt("Enter new todo")
+    //add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodo() {
+    //ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+        
+    //delete that todo
+    todos.splice(index,1);
+    console.log("Deleted Todo");
+}
